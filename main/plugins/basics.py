59407e0abb466eaf72c7b1e71bf8aa498972f1b7
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2021-12-31 00:58:15 +0530

    Update basics.py

--- main/plugins/basics.py
+++ main/plugins/basics.py
@@ -3,3 +3,31 @@
 
 from .. import CA
 from telethon import events
+from main.plugins.stuff import chat
+
+@CA.on(events.NewMessage(incoming=True, from_users=AUTH_USERS , pattern="!rly"))
+async def reply(event):
+    x = await event.get_reply_message()
+    if not x:
+        return await event.reply("No message found!")
+    try:
+        reply_id = int(float((event.text).split(" ")[1]))
+    except Exception:
+        return await event.reply("No msg id found.")
+    await event.client.send_message(chat, x, reply_to=reply_id)
+    
+@CA.on(events.NewMessage(incoming=True, from_users=AUTH_USERS , pattern="!msg"))
+async def msg(event):   
+    x = await event.get_reply_message()
+    if not x:
+        return await event.reply("No message found!")
+    try:
+        msg_id = int(float((event.text).split(" ")[1]))
+    except Exception:
+        return await event.reply("No msg id found.")
+    await event.client.send_message(msg_id, x)
+    
+    
+    
+    
+    
