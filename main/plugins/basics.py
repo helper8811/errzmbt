7fa1637a7934187e21a18488bd77d6dedb7cbae4
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-02 09:59:50 +0530

    Update basics.py

--- main/plugins/basics.py
+++ main/plugins/basics.py
@@ -2,9 +2,10 @@
 #github.com/vasusen-code
 
 from .. import bot as CA, AUTH_USERS
+from .. import CHAT as chat
 from telethon import events
-
-chat = '@teamdrone'
+from telethon.errors.rpcerrorlist import UserNotParticipantError
+from telethon.tl.functions.channels import GetParticipantRequest
 
 @CA.on(events.NewMessage(incoming=True, from_users=AUTH_USERS , pattern="!rly"))
 async def reply(event):
@@ -28,7 +29,43 @@ async def msg(event):
         return await event.reply("No msg id found.")
     await event.client.send_message(msg_id, x)
     
-    
-    
+  async def get_joiner(id):
+    ok = True
+    try:
+        await Asst(GetParticipantRequest(channel=int(chat), participant=id))
+        ok = True
+    except UserNotParticipantError:
+        ok = False
+    return ok
+
+#Welcome-------------------------------------------------------------------------
+
+@CA.on(events.ChatAction(chats=chat))
+
+async def get_joiner(id):
+    x = True
+    try:
+        await Asst(GetParticipantRequest(channel=int(chat), participant=id))
+        x = True
+    except UserNotParticipantError:
+        x = False
+    return x
+
+async def joined(event):
+    if event.user_joined or event.user_added:
+        user = await event.get_user()
+    firstname = user.first_name
+    mention = user.username
+    if not mention:    
+        mention = f"[{firstname}](tg://user?id={user.id})"
+    x = await get_joiner(user.id)
+    if x is True:
+        await CA.send_message(chat , f'`ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴅʀᴏɴᴇ sᴜᴘᴘᴏʀᴛ`\n\n👤 : {mention}\n\n🆔 : `{user.id}`\n\nGreetings by @MaheshChauhan.',
+                             buttons=[
+                                 [
+                                  Button.url("Association", url="t.me/thechariotoflight"),
+                                  Button.url("Updates", url="t.me/DroneBots")]])                                              
+    else:
+        return  
     
     
