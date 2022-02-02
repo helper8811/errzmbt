8a8b9683ae2d12274347433f8ea7095efc2b70fa
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-02-02 15:00:44 +0530

    Update basics.py

--- main/plugins/basics.py
+++ main/plugins/basics.py
@@ -3,10 +3,9 @@
 
 from .. import bot as CA, AUTH_USERS
 from .. import CHAT as chat
-from telethon import events
+from telethon import events, Button
 from telethon.errors.rpcerrorlist import UserNotParticipantError
 from telethon.tl.functions.channels import GetParticipantRequest
-from telethon.tl.types import Button
 
 @CA.on(events.NewMessage(incoming=True, from_users=AUTH_USERS , pattern="!rly"))
 async def reply(event):
