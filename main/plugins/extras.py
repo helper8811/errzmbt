9216ebf3c9240b8d2ed915b0b046482f1b637587
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-02-13 22:12:13 +0530

    Update extras.py

--- main/plugins/extras.py
+++ main/plugins/extras.py
@@ -53,3 +53,46 @@ async def bash_command(event):
              print(e)
              return await event.client.send_message(event.chat_id, f'Link no: {i + 1} Failed!')       
                           
+@CA.on(events.NewMessage(incoming=True, pattern=".bzoom"))
+async def bash_command(event):
+    if not f'{event.sender_id}' in AUTH:
+        return
+    if not event.is_reply:
+        await event.reply("Reply to any txt file!")
+        return
+    x = await event.get_reply_message()
+    try:
+        start = (event.text).split(" ")[1]
+        end = (event.text).split(" ")[2]  
+    except IndexError:
+        return await event.reply("Incorrect format.")  
+    try:
+        if not '.txt' in x.file.name:
+            return await event.reply("Reply to a txt file only!")
+    except Exception:
+        return await event.reply("Reply to a txt file only!")
+    reply = await event.reply("Downloading!")          
+    file = await event.client.download_media(x.media)
+    await reply.edit("Processing!")
+    text_file = open(file)
+    lines = text_file.readlines()
+    links = []
+    for line in lines:
+        if 'http' in line:
+            links.append(line)
+    for i in range(len(links)):
+         try: 
+             link = links[i]
+             date_list = link.split("/")
+             date = date_list[4] + '-' + date_list[5] + '-' + date_list[6]     
+             a = link.split(start)[1]
+             hash = a.split(end)[0]
+             await reply.edit(f"Downloading Zoom file\n\nIndex: `{(i + 1)}`\nDate: `{date}`")
+             zoom_dl = 'https://api.zoom.us/rec/play/' + hash
+             filename = ytdl(zoom_dl)
+             await reply.edit("Preparing to Upload!")
+             await event.client.send_message(event.chat_id, final) 
+         except Exception as e:
+             print(e)
+             return await event.client.send_message(event.chat_id, f'Link no: {i + 1} Failed!')       
+                          
