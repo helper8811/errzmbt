e79260943af6328a04aa2f73ff50d6906bd90e5c
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-02-13 16:34:34 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -13,7 +13,7 @@ API_ID = config("API_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
 SESSION = config("SESSION", default=None)
-AUTH_USERS = config("AUTH_USERS", default=None, cast=int)
+AUTH_USERS = config("AUTH_USERS", default=None)
 CHAT = config("CHAT", default=None, cast=int)
 
 AUTH = []
