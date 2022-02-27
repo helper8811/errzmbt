5fa351a450d9701baec839bcc8800cbd86e8b052
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-02-27 15:16:07 +0530

    Update dl.py

--- main/plugins/dl.py
+++ main/plugins/dl.py
@@ -6,10 +6,12 @@ from telethon import events
 @bot.on(events.NewMessage(incoming=True, pattern="!dl1"))
 async def dl1(event):
     x = await event.get_reply_message()
+    reply = await event.reply("Downloading!")
     msg = await pbot.get_messages(event.sender_id, x.id)
-    await event.edit(await pbot.download_media(msg))
+    await reply.edit(await pbot.download_media(msg))
     
 @bot.on(events.NewMessage(incoming=True, pattern="!dl2"))
 async def dl2(event):
     x = await event.get_reply_message()
-    await event.edit(await pbot.download_media(x.media))
+    reply = await event.reply("Downloading!")
+    await reply.edit(await pbot.download_media(x.media))
