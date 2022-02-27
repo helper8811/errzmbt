13a755ab0c67efedc8a053bc4454c56a7654c985
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-02-27 15:11:55 +0530

    Update dl.py

--- main/plugins/dl.py
+++ main/plugins/dl.py
@@ -3,13 +3,13 @@ from .. import bot, pbot
 from pyrogram import Client
 from telethon import events
 
-@bot.on(events.NewMessage(incoming=True, from_users=AUTH_USERS , pattern="!dl1"))
+@bot.on(events.NewMessage(incoming=True, pattern="!dl1"))
 async def dl1(event):
     x = await event.get_reply_message()
     msg = await pbot.get_messages(event.sender_id, x.id)
     await event.edit(await pbot.download_media(msg))
     
-@bot.on(events.NewMessage(incoming=True, from_users=AUTH_USERS , pattern="!dl2"))
+@bot.on(events.NewMessage(incoming=True, pattern="!dl2"))
 async def dl2(event):
     x = await event.get_reply_message()
     await event.edit(await pbot.download_media(x.media))
