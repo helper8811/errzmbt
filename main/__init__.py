5d3f663c6e94fd90962e816864c146bdfd2ca179
helper8811 <108834078+helper8811@users.noreply.github.com>
2023-02-01 20:54:27 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -10,13 +10,13 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # Basics
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-AU = config("AUTH_USERS", default=None)
-CHAT = config("CHAT", default=None, cast=int)
-LCHAT = config("LCHAT", default=None)
+API_ID = config("12665045", default=None, cast=int)
+API_HASH = config("d437379dcebbc0a422211503956d8194", default=None)
+BOT_TOKEN = config("6009441071:AAHoB7Jf3CRLTcJlsEcUyVzPKm7I-YwLu7I", default=None)
+SESSION = config("AQDCiriaFyuy_nKp5UbrtyDCIiZ9R6jWnT0GNxo3e4xL_LZ6RQrlzSiupopLvHzd4SgjW1zZx5JRFeQqHhfTUz2-zlYRkx53rj7Us0pGdIKiQCQDw6V0m9kOVtYR4QsUjt5PcUmL9JFHJH2GXSf3NAXV0-XNaliY_vphUnYI7zQkBjgd7a5ktXQxnfq-kBswHDEldaqhs-xA52kVpR37h41VRFiPV2WLwRqOb7hhTuNT6pk5A85g1d3hEqQKx1KEb3hx7GMxLYqsIhuLXdzphlu7ozAElR36bOCwnivhLYlDOLYqWtUh1NxMAST4voPwgbF-4Bv88dRjyu56eMAyzIm6AAAAATs_hjgA", default=None)
+AU = config("5288986168", default=None)
+CHAT = config("1538922643", default=None, cast=int)
+LCHAT = config("1538922643", default=None)
     
 #connection
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
