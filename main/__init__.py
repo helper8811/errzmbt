59271f060ab638313077b5aeff43f096ea01261a
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2023-02-03 19:01:12 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -13,7 +13,6 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 API_ID = 12665045
 API_HASH = "d437379dcebbc0a422211503956d8194"
 BOT_TOKEN = "6009441071:AAHoB7Jf3CRLTcJlsEcUyVzPKm7I-YwLu7I"
-SESSION = "SESSION", default=None
 AU = 5288986168
 CHAT = int("-1001538922643")
 LCHAT = int("-1001538922643")
